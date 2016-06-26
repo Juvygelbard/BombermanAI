06648d00285353de10d0140b8bba8c774068e676
YUVAL <juvygelbard@gmail.com>
2016-06-26 13:59:55 +0300

    parallel

---
+++ servers.bat
@@ -0,0 +1,7 @@
+start node BombermanAI\Server\server.js 8037
+start node BombermanAI\Server\server.js 8038
+start node BombermanAI\Server\server.js 8039
+start node BombermanAI\Server\server.js 8040
+start node BombermanAI\Server\server.js 8041
+start node BombermanAI\Server\server.js 8042
+exit
